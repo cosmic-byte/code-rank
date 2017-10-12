29db421a57fc2260de65b8fc1e88d6af58fcd338
gregobinna <gregobinna@gmail.com>
2017-10-12 13:04:49 +0100

    add elastic dependency to pom.xml

--- src/main/java/com/coderank/io/CodeRankApplication.java
+++ src/main/java/com/coderank/io/CodeRankApplication.java
@@ -1,9 +1,12 @@
 package com.coderank.io;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CodeRankApplication {
 
 	public static void main(String[] args) {
